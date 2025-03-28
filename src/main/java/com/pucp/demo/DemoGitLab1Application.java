258e1b9bd13588ba463fa2bd92fcc93babaa4e63
Lewis Alexis Rengifo Pizango <l.rengifo@pucp.edu.pe>
2025-03-27 19:58:04 -0500

    first commit

---
+++ src/main/java/com/pucp/demo/DemoGitLab1Application.java
@@ -0,0 +1,13 @@
+package com.pucp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitLab1Application.class, args);
+	}
+
+}
